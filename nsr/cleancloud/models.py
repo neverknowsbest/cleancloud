49ce55a21d3827be2f4780faba8e6b0484be47a2
Alfredo <bonds0097@gmail.com>
2013-04-07 21:44:35 -0400

    Ticket #47:
    Added company name and phone number to user profile and edit profile pages.
    
    NOTE: UserProfile model was updated with two new fields: 'company' and 'phone'. Refer to models.py to make necessary Database Changes.

--- nsr/cleancloud/models.py
+++ nsr/cleancloud/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
 from django.contrib.auth.models import User
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
@@ -94,6 +94,10 @@ class UserProfile(models.Model):
 	city = models.CharField(max_length=100, default="")
 	state = models.CharField(max_length=100, default="")
 	zipcode = models.IntegerField(max_length=5, default=0)
+	company = models.CharField(max_length=100, default='')
+	phone = models.CharField(max_length=10, default=0, 
+								help_text='Numbers only, no spaces or dashes.',
+								validators=[RegexValidator(regex='\d{10}')],)
 	
 	def __unicode__(self):  
 		return "%s's profile %s" % (self.user, self.first_name)
