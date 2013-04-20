6b839f53d02c3c47e8f2fdfa29dccf65cc4d92ac
Alfredo <bonds0097@gmail.com>
2013-04-20 15:26:23 -0400

    Ticket #59:
    Changed how phone number and zipcode are handled in form. Defaults removed. Blank values now accepted. Modified form to include rather than exclude fields (per Django best practices).

--- nsr/dedool_profile/forms.py
+++ nsr/dedool_profile/forms.py
@@ -1,7 +1,12 @@
 from django import forms
+
 from dedool_profile.models import UserProfile
 
+
 class UserProfileForm(forms.ModelForm):
-	class Meta:
-		model = UserProfile
-		exclude = ('jobs', 'user', 'street_address_2')
+    error_css_class = 'errorField'
+
+    class Meta:
+        model = UserProfile
+        fields = ('first_name', 'last_name', 'company', 'phone', 'street_address_1',
+                  'street_address_2', 'city', 'state', 'zipcode', )
--- nsr/dedool_profile/models.py
+++ nsr/dedool_profile/models.py
@@ -11,15 +11,15 @@ class UserProfile(models.Model):
     jobs = models.ManyToManyField(Job)
     #1 profile per user, 1 user per profile
     user = models.OneToOneField(User)
-    first_name = models.CharField(max_length=100, default="", blank=True)
-    last_name = models.CharField(max_length=100, default="", blank=True)
-    street_address_1 = models.CharField(max_length=100, default="", blank=True)
-    street_address_2 = models.CharField(max_length=100, default="", blank=True)
-    city = models.CharField(max_length=100, default="", blank=True)
-    state = models.CharField(max_length=100, default="", blank=True)
+    first_name = models.CharField(max_length=100, blank=True, verbose_name=u'First Name')
+    last_name = models.CharField(max_length=100, blank=True, verbose_name=u'Last Name')
+    street_address_1 = models.CharField(max_length=100, blank=True)
+    street_address_2 = models.CharField(max_length=100, blank=True)
+    city = models.CharField(max_length=100, blank=True)
+    state = models.CharField(max_length=100, blank=True)
     zipcode = models.IntegerField(max_length=5, default=0, blank=True)
-    company = models.CharField(max_length=100, default='', blank=True)
-    phone = models.CharField(max_length=10, default=0,
+    company = models.CharField(max_length=100, blank=True)
+    phone = models.CharField(max_length=10, verbose_name=u'Phone Number',
                              help_text='Numbers only, no spaces or dashes.',
                              validators=[RegexValidator(regex='\d{10}',
                                                         message='Must consist of exactly 10 digits.')],
