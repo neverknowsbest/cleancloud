396dd07c812612a9575332cb83905e42e99356b3
Alfredo <bonds0097@gmail.com>
2013-04-21 01:36:14 -0400

    Ticket #61: Add @login_required to Functions View
    Added login_required() function to base Functions URL.

--- nsr/nsr/urls.py
+++ nsr/nsr/urls.py
@@ -1,45 +1,46 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='cleancloud/index.html'), name="index"),
-	url(r'^account/', include('registration.backends.default.urls')),
+    url(r'^account/', include('registration.backends.default.urls')),
     url(r'^admin/', include(admin.site.urls)),
-	url(r'^functions/$', TemplateView.as_view(template_name='cleancloud/functions.html'), name="functions"),
+    url(r'^functions/$', login_required(TemplateView.as_view(template_name='cleancloud/functions.html')), name="functions"),
 )
 
 urlpatterns += patterns('dedool_jobs.views',
-	url(r'^continue/(\d*)/$', 'continue_job'),
-	url(r'^job_history/$', 'job_history'),
+    url(r'^continue/(\d*)/$', 'continue_job'),
+    url(r'^job_history/$', 'job_history'),
 )
 
 urlpatterns += patterns('dedool_files.views',
-	url(r'^files/$', 'files'),
+    url(r'^files/$', 'files'),
 )
 
 urlpatterns += patterns('dedool_functions.views',
-	url(r'^start/$', 'start'),
-	url(r'^upload/(\d*)/$', 'upload'),
-	url(r'^select/(\d*)/$', 'select'),
-	url(r'^review/(\d*)/$', 'review'),
-	url(r'^configure/(\d*)/$', 'configure'),
-	url(r'^results/(\d*)/$', 'results'),
-	url(r'^status/$', 'status_form'),
-	url(r'^status/(\d*)/$', 'status'),
+    url(r'^start/$', 'start'),
+    url(r'^upload/(\d*)/$', 'upload'),
+    url(r'^select/(\d*)/$', 'select'),
+    url(r'^review/(\d*)/$', 'review'),
+    url(r'^configure/(\d*)/$', 'configure'),
+    url(r'^results/(\d*)/$', 'results'),
+    url(r'^status/$', 'status_form'),
+    url(r'^status/(\d*)/$', 'status'),
 )
 
 urlpatterns += patterns('cleancloud.ajax_views',
-	url(r'^check/(\d*)/$', 'check'),
-	url(r'^cancel/(\d*)/$', 'cancel'),
-	url(r'^edit/(\d*)/$', 'edit'),
-	url(r'^revert/(\d*)/(\d*-\d*)/$', 'revert'),
-	url(r'^remove/(\d*)/$', 'remove'),
-	url(r'^hide/(\d*)/$', 'hide')
+    url(r'^check/(\d*)/$', 'check'),
+    url(r'^cancel/(\d*)/$', 'cancel'),
+    url(r'^edit/(\d*)/$', 'edit'),
+    url(r'^revert/(\d*)/(\d*-\d*)/$', 'revert'),
+    url(r'^remove/(\d*)/$', 'remove'),
+    url(r'^hide/(\d*)/$', 'hide')
 )
 
 urlpatterns += patterns('dedool_profile.views',
-	url(r'^account/profile/$', 'profile', name='profile'),
+    url(r'^account/profile/$', 'profile', name='profile'),
 )
